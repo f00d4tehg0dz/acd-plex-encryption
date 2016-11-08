131c29f4fd72260b434ff4776d06841426b9df1e
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-08 18:22:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@
 mkdir -p $ACD_PLEX/.local-sorted $ACD_PLEX/local-sorted $ACD_PLEX/.acd-sorted $ACD_PLEX/acd-sorted
 
 # On first run, you'll need to setup EncFS. Just use the defaults.
-
 encfs $ACD_PLEX/.local-sorted $ACD_PLEX/local-sorted
+
+# Now unmount it
+fusermount -u $ACD_PLEX/local-sorted
+
+# Mount with ENCFS configured
+
+ENCFS6_CONFIG='/home/owner/Plex/encfs.xml' encfs /home/owner/Plex/.local-sorted /home/owner/Plex/local-sorted
 
 # Mount drive
 
@@ -16,7 +22,7 @@ acd_cli mount $ACD_PLEX/.acd-sorted/
 mv $ACD_PLEX/.local-sorted/.encfs6.xml $ACD_PLEX/encfs6.xml
 
 # Link Drive paths, and allow other in case you runinto issues with Plex not reading acd-sorted
-
+# Mounting encfs over acd_cli
 sudo ENCFS6_CONFIG='$ACD_PLEX/encfs.xml' encfs -o allow_other $ACD_PLEX/.acd-sorted $ACD_PLEX/acd-sorted
 
 # If plex doesn't recognize your folders after Sync. Give the folder the same useraccount you used for PLEX and setting up your environment.
