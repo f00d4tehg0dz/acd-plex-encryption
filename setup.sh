54105a850ad656aa4c3ad41b0b9a38db797e8f7c
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-08 12:46:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,10 @@
 # Referenced from - https://gist.github.com/samatjain/987f946b29724401148c which is referenced from https://amc.ovh/2015/08/14/mounting-uploading-amazon-cloud-drive-encrypted.html
 
-mkdir -p \
-$ACD_PLEX/.local-sorted \
-$ACD_PLEX/local-sorted \
-$ACD_PLEX/.acd-sorted \
-$ACD_PLEX/acd-sorted
+mkdir -p $ACD_PLEX/.local-sorted $ACD_PLEX/local-sorted $ACD_PLEX/.acd-sorted $ACD_PLEX/acd-sorted
 
 # On first run, you'll need to setup EncFS. Just use the defaults.
 
-encfs \
-$ACD_PLEX/.local-sorted \
-$ACD_PLEX/local-sorted
+encfs $ACD_PLEX/.local-sorted $ACD_PLEX/local-sorted
 
 # Mount drive
 acd_cli mount $ACD_PLEX/.acd-sorted/
@@ -18,9 +12,7 @@ acd_cli mount $ACD_PLEX/.acd-sorted/
 # Backup key & configuration for safe-keeping
 # TODO: this shouldn't be in ~/.local-sorted cache/
 
-ln \
-$ACD_PLEX/.local-sorted/.encfs6.xml \
-$ACD_PLEX/encfs6.xml
+ln $ACD_PLEX/.local-sorted/.encfs6.xml $ACD_PLEX/encfs6.xml
 
 # Link Drive paths, and allow other in case you runinto issues with Plex not reading acd-sorted
 
