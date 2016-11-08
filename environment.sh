62b64e8e760b9d750a441f7216b01341411ea51d
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-08 12:59:34 -0500

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,2 +1,26 @@
-#environment setup
-export ACD_PLEX=$HOME/Plex/
+# --- Pre Req --- #
+
+# Python 3
+
+# FUSE kernel module* (Untested)
+
+sudo apt-get install fuseiso
+sudo modprobe fuse
+sudo adduser <your login username> fuse
+sudo adduser bgates fuse
+
+# Encfs (Untested)
+
+sudo apt-get install encfs
+
+
+# acd_cli (Untested)
+
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade git+https://github.com/yadayada/acd_cli.git
+
+# --- END --- #
+
+# Save time and shorten the directory in your commandline. 
+
+export ACD_PLEX=$HOME/Plex
