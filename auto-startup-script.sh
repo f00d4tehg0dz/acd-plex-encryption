2ff5ddc0293c77c7504f3b822405bf0693e1fb3d
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-14 09:34:29 -0500

    Update auto-startup-script.sh

--- auto-startup-script.sh
+++ auto-startup-script.sh
@@ -8,13 +8,11 @@ pass=''
 #sudo fusermount -u /home/plex/local-sorted
 
 cd /
-sudo acd_cli mount home/plex/.local-sorted
-echo $sudopass
+acd_cli mount home/plex/.local-sorted
 cd home/plex/																
-sudo ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.local-sorted /home/plex/local-sorted
+ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.local-sorted /home/plex/local-sorted
 echo $pass
-sudo acd_cli mount home/plex/.acd-sorted
-echo $sudopass
-sudo ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.acd-sorted /home/plex/acd-sorted
+acd_cli mount home/plex/.acd-sorted
+ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.acd-sorted /home/plex/acd-sorted
 echo $pass
 acd_cli sync
