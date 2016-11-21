4bb72d13429b9c42ad6f87c612eb997854bd9cf9
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-21 10:10:45 -0500

    Update auto-startup-script.sh

--- auto-startup-script.sh
+++ auto-startup-script.sh
@@ -1,5 +1,5 @@
-sudopass=''
-pass=''
+#sudopass=''
+#pass=''
 
 #kill any previously mounted pieces
 #sudo fusermount -u /home/plex/.local-sorted
@@ -7,12 +7,17 @@ pass=''
 #sudo fusermount -u /home/plex/.acd-sorted
 #sudo fusermount -u /home/plex/local-sorted
 
-cd /
-acd_cli mount home/plex/.local-sorted
-cd home/plex/																
-ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.local-sorted /home/plex/local-sorted
-echo $pass
-acd_cli mount home/plex/.acd-sorted
-ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.acd-sorted /home/plex/acd-sorted
-echo $pass
+#cd /
+#acd_cli mount home/plex/.local-sorted
+#cd home/plex/																
+#ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.local-sorted /home/plex/local-sorted
+#echo $pass
+#acd_cli mount home/plex/.acd-sorted
+#ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -o nonempty /home/plex/.acd-sorted /home/plex/acd-sorted
+#echo $pass
+#acd_cli sync
+
+cat ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -S --public /home/plex/.local-sorted /home/plex/local-sorted -- -o gid=1001
 acd_cli sync
+acd_cli mount /home/plex/.acd-sorted/ --allow-other
+cat ENCFS6_CONFIG='/home/plex/encfs.xml' encfs -S --public /home/plex/.acd-sorted /home/plex/acd-sorted -- -o gid=1001 -o allow_other
