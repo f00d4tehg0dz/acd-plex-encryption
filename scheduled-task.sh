e2251dc8035560281b7ea0e0b2cb310256276d00
f00d4tehg0dz <adrianvfx@gmail.com>
2016-11-21 10:11:58 -0500

    Update scheduled-task.sh

--- scheduled-task.sh
+++ scheduled-task.sh
@@ -1,5 +1,6 @@
 # Periodically run:
 # (will prevent uploading of key and config to cloud)
 # Ideally encfs6.xml should have been moved from setup.sh
-sudo acd_cli upload -x 4 $ACD_PLEX/.local-sorted/* /
+#sudo acd_cli upload -x 4 $ACD_PLEX/.local-sorted/* /
+sudo acd_cli ul $ACD_PLEX/.local-sorted /Archives
 sudo acd_cli sync
